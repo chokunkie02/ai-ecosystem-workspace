a04f91f47c2c4e87f5d1e6b029d535aa48969b42
chokunkie02 <aumkanitta02@gmail.com>
2026-07-30 13:28:46 +0700

    เพิ่มบริการ services layer และตั้งค่า CORS middleware

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from backend.routers import item_router
 from backend.core.config import settings
 
 app = FastAPI(title=settings.project_name, version=settings.version)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(item_router.router)
 
 @app.get("/")
