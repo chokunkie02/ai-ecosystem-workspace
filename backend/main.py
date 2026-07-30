a8fc62485fc6e93ff3dea79db6cc6b5bf3fb3b08
chokunkie02 <chokunkie02@users.noreply.github.com>
2026-07-30 17:56:57 +0700

    อัพเดตให้เหมาะสำหรับai llm และระบบsse

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from backend.routers import item_router
-from backend.core.config import settings
+from routers.ai_router import ai_router
 
-app = FastAPI(title=settings.project_name, version=settings.version)
+app = FastAPI(title="AI LLM Inference API")
 
 app.add_middleware(
     CORSMiddleware,
@@ -12,8 +11,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-app.include_router(item_router.router)
+
+app.include_router(ai_router)
 
 @app.get("/")
-def root():
-    return {"message": f"Welcome to {settings.project_name}"}
+async def root():
+    return {"message": "Welcome to AI LLM Inference API"}
